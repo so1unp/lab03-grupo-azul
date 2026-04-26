0682f36cd823b7f6ee87895eccd65911698dd703
Enzo Riera <enzoriera987@gmail.com>
2026-04-26 18:50:13 -0300

    arreglo de warning y errores

--- forkprio.c
+++ forkprio.c
@@ -1,16 +1,22 @@
 #define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/times.h>      // times()
+#include <time.h>         // clock_gettime()
 #include <unistd.h>
 #include <signal.h>        // sigaction()
 #include <sys/wait.h>      // wait()
+#include <sys/resource.h> // getpriority(), setpriority()
 
 
 
 void manejador_kill(int sig) {
-    struct tms buf; times(&buf);
-    printf("Child %d (nice %2d) : \t%3li\n", getpid(), getpriority(), buf);
+    (void)sig; // Evitar advertencia de variable no utilizada
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    long int ms = (long int)(now.tv_sec * 1000L + now.tv_nsec / 1000000L);
+    printf("Child %d (nice %2d) : \t%ld ms\n", getpid(), getpriority(PRIO_PROCESS, 0), ms);
     exit(EXIT_SUCCESS);
 }
 
@@ -23,17 +29,28 @@ int busywork(void)
     }
 }
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {   
+    struct sigaction sa;
 
-    if (argc != 3) {
+    sa.sa_handler = manejador_kill; // Asignar la función
+    sigemptyset(&sa.sa_mask);       // No bloquear otras señales durante el manejo
+    sa.sa_flags = 0;                // Configuración por defecto
+
+    if (sigaction(SIGTERM, &sa, NULL) == -1) {
+
+        perror("Error registrando sigaction");
+        return  1;
+    }
+
+    if (argc != 4) {
         fprintf(stderr, "Uso: %s [s|f] origen destino\n", argv[0]);
         return EXIT_FAILURE;
     }
     
 
-    int priority = argv[2];
-    int childsWants = argv[0];
+    int priority = atoi(argv[3]);
+    int childsWants = atoi(argv[1]);
     int childsId[childsWants];
 
     for (int i = 0; i < childsWants; i++ ) {
@@ -42,33 +59,22 @@ int main(int argc, int *argv[])
             if(priority){
                 if (i>19)
                 {
-                    nice(19);    /* code */
+                    setpriority(PRIO_PROCESS, 0, 19);    /* code */
                 }else{
-                    nice(i);
+                    setpriority(PRIO_PROCESS, 0, i);
                 }
                                  
             } 
-             struct sigaction sa;
-
-            sa.sa_handler = manejador_kill; // Asignar la función
-            sigemptyset(&sa.sa_mask);       // No bloquear otras señales durante el manejo
-            sa.sa_flags = 0;                // Configuración por defecto
-
-            if (sigaction(SIGTERM, &sa, NULL) == -1) {
-
-                perror("Error registrando sigaction");
-                return  1;
-            }
-
+                    
             busywork();
             exit(EXIT_SUCCESS);
             
            // i = childsWants; Para salir del bucle
         }
     }
-    if (argv[1] > 0 ){
+    if (atoi(argv[2]) > 0 ){
 
-        sleep(argv[1]);
+        sleep((unsigned int)(atoi(argv[2])));
             
 
     }else{
